9e88396a963e04c3fa5b17440914da9eacc9fc1b
Maxim Vintsyun <maxes2000@yandex.ru>
2023-10-14 10:48:40 +0500

    feat: добавлены роуты и контроллеры

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6526dd743fe27336b573e3cb',
+  };
+
+  next();
 });
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.listen(PORT);
